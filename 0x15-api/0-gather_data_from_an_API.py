ab29cbc6bc820bf9576ca3db4d7bf78c10bd90ec
Edison Girado A <1470@Holbertonschool.com>
2020-09-14 17:44:59 -0500

    employee information and his/her tasks.

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""prints information about employee and his/her tasks using employee id."""
+import requests
+from sys import argv
+
+emp_id = argv[1]
+route = "https://jsonplaceholder.typicode.com/users/{}".format(emp_id)
+r = requests.get(route)
+usr_info = r.json()
+emp_name = usr_info.get('name')
+
+route = "https://jsonplaceholder.typicode.com/users/{}/todos".format(emp_id)
+r = requests.get(route)
+tasks = r.json()
+
+done_tasks = 0
+for task in tasks:
+    if task.get('completed') is True:
+        done_tasks += 1
+
+tot_tasks = len(tasks)
+
+print("Employee {} is done with tasks({}/{}):"
+      .format(emp_name, done_tasks, tot_tasks))
+
+for task in tasks:
+    if task.get('completed') is True:
+        print("\t {}".format(task.get('title')))
