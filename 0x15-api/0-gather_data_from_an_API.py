6ce7b76ee9a5e519030310e8739a17b2d32f9774
Edison Girado A <1470@Holbertonschool.com>
2020-09-14 17:49:42 -0500

    employee information and his/her tasks.

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,26 +3,27 @@
 import requests
 from sys import argv
 
-emp_id = argv[1]
-route = "https://jsonplaceholder.typicode.com/users/{}".format(emp_id)
-r = requests.get(route)
-usr_info = r.json()
-emp_name = usr_info.get('name')
+if __name__ == '__main__':
+    emp_id = argv[1]
+    route = "https://jsonplaceholder.typicode.com/users/{}".format(emp_id)
+    r = requests.get(route)
+    usr_info = r.json()
+    emp_name = usr_info.get('name')
 
-route = "https://jsonplaceholder.typicode.com/users/{}/todos".format(emp_id)
-r = requests.get(route)
-tasks = r.json()
+    route = "https://jsonplaceholder.typicode.com/users/{}/todos".format(emp_id)
+    r = requests.get(route)
+    tasks = r.json()
 
-done_tasks = 0
-for task in tasks:
-    if task.get('completed') is True:
-        done_tasks += 1
+    done_tasks = 0
+    for task in tasks:
+        if task.get('completed') is True:
+            done_tasks += 1
 
-tot_tasks = len(tasks)
+    tot_tasks = len(tasks)
 
-print("Employee {} is done with tasks({}/{}):"
-      .format(emp_name, done_tasks, tot_tasks))
+    print("Employee {} is done with tasks({}/{}):"
+          .format(emp_name, done_tasks, tot_tasks))
 
-for task in tasks:
-    if task.get('completed') is True:
-        print("\t {}".format(task.get('title')))
+    for task in tasks:
+        if task.get('completed') is True:
+            print("\t {}".format(task.get('title')))
