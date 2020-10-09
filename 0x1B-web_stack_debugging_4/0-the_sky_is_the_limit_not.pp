aee220264848109d03fe2ea0a7835f9a0ebf08c6
Edison Girado A <1470@Holbertonschool.com>
2020-10-08 20:03:27 -0500

    The sky is the limit

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,12 @@
+# Sky is the limit
+
+exec {'nginx':
+    command => "sed -i 's/15/1000/g' /etc/default/nginx",
+    path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
+}
+
+exec {'restart':
+    command => 'service nginx restart',
+    path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
+}
+
